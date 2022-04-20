a98b98721aaac58d6a868fffe8fbea29eb3f275b
Juan David <juandavid@Juans-MacBook-Pro.local>
2022-04-20 09:34:46 -0500

    Modificacion de php y laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Posty</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <title>Chiwi Academy</title>
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body class="bg-gray-200">
-        <nav class="p-6 bg-white flex justify-between mb-6">
+        <nav class="p-6 bg-indigo flex justify-between mb-6">
             <ul class="flex items-center">
                 <li>
                     <a href="/" class="p-3">Home</a>
